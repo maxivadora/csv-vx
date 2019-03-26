dea644ccb9abdee543091bbf29112d22970a1e2a
Maxi Vadora <maxivadora@gmail.com>
2019-03-26 12:29:50 -0300

    added active_admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'vx@vx.com', password: 'vxvxvx', password_confirmation: 'vxvxvx') if Rails.env.development?
